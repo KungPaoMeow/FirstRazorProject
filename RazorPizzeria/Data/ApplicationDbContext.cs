aab73c12058ed7d606c4a4d52bc7b804b891bba5
KungPaoMeow <johnli8642@gmail.com>
2024-05-31 08:34:13 -0400

    push project

---
+++ RazorPizzeria/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿//using Microsoft.EntityFrameworkCore;
+
+namespace RazorPizzeria.Data
+{
+    // This file is for If this was used with SQL database with EntityFrameworkCore packages
+    public class ApplicationDbContext // : DbContext
+    {
+        // public DbSet<PizzaOrder> PizzaOrders { get; set; }
+
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+        //    : base(options)
+        //{
+
+        //}
+
+
+        // (call `add-migration addedPizzaOrder` in Package Manager to generate a migrator, then `update-database`)
+    }
+}
---
+++ RazorPizzeria/Models/PizzaOrder.cs
@@ -0,0 +1,9 @@
+﻿namespace RazorPizzeria.Models
+{
+    public class PizzaOrder
+    {
+        public int Id { get; set; }     // This file is for If this was used with SQL database with EntityFrameworkCore
+        public string PizzaName { get; set; }
+        public float BasePrice { get; set; }
+    }
+}
